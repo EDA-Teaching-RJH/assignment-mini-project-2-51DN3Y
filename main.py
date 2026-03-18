39780c75ed885a64f105465937a0e7cec5b6c3a6
Sidney Beacher <sidneymb11@gmail.com>
2026-03-18 14:52:29 +0000

    Created a menu to display options for the user to interact with the training log.

---
+++ main.py
@@ -0,0 +1,8 @@
+def display_menu():
+    print("\n -- Rowing Training Log --")
+    print("1. Add session")
+    print("2. Remove session")
+    print("3. View sessions")
+    print("4. Search sessions")
+    print("5. Save and Exit")
+display_menu()
