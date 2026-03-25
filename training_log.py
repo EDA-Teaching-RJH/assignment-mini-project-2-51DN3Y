f15c0a2df714539d684e6c0fd6aa82d58cd0cbbc
Sidney Beacher <sidneymb11@gmail.com>
2026-03-25 20:33:38 +0000

    Created load log function with error handling

--- training_log.py
+++ training_log.py
@@ -34,3 +34,15 @@ class TrainingLog:
                 results.append(session)
 
         return results
+    
+    def load_from_csv(self, training_results):
+        try:
+            with open(training_results, "r") as file:
+                reader = csv.reader(file)
+                for row in reader:
+                    if len(row) == 4:
+                        session = Session(row[0], row[1], row[2], row[3])
+                        self.add_session(session)
+        
+        except FileNotFoundError:
+            print("No existing training log found.")
