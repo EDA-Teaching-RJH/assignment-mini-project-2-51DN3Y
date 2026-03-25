507ccddf7044286808d76f70b5c70461a89247ec
Sidney Beacher <sidneymb11@gmail.com>
2026-03-25 15:49:47 +0000

    Creation of session class

--- training_log.py
+++ training_log.py
@@ -1,3 +1,9 @@
 import csv
 import re
 
+class Session:
+    def __init__ (self, date, distance, time, notes):
+        self.date = date
+        self.distance = distance
+        self.time = time
+        self.notes = notes
