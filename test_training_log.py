1ed9ea6f739c2180e05fed6c49d31027405e79c9
Sidney Beacher <sidneymb11@gmail.com>
2026-03-26 18:16:31 +0000

    Added testing for removing session

--- test_training_log.py
+++ test_training_log.py
@@ -9,3 +9,12 @@ def test_add_session():
     s = Session("01/01/2022", 10, "50:00", "Test session")
     log.add_session(s)
     assert len(log.get_all_sessions()) == 1
+
+def test_remove_session():
+    log = TrainingLog()
+    s = Session("01/01/2022", 10, "50:00", "Test session")
+    log.add_session(s)
+    log.remove_session(0)
+    assert len(log.get_all_sessions()) == 0
+
+
