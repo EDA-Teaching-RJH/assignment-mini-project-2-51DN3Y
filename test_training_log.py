b72f97aef7022449424905b1729bf5534fa0f834
Sidney Beacher <sidneymb11@gmail.com>
2026-03-26 18:14:00 +0000

    Added testing for add_session

--- test_training_log.py
+++ test_training_log.py
@@ -4,3 +4,8 @@ def test_pace():
     s = Session("01/01/2022", 10, "50:00", "Test session")
     assert s.get_pace() == 5.0
 
+def test_add_session():
+    log = TrainingLog()
+    s = Session("01/01/2022", 10, "50:00", "Test session")
+    log.add_session(s)
+    assert len(log.get_all_sessions()) == 1
